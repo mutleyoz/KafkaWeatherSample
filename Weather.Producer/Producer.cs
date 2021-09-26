1c9e8bb26adcfd6f71934be41cb8d2e2ce92874a
Sean Campbell <scampbell@mutley.net.au>
2021-09-26 12:31:15 +1000

    Successfully publishing messages

--- Weather.Producer/Producer.cs
+++ Weather.Producer/Producer.cs
@@ -1,11 +1,9 @@
 ﻿using Confluent.Kafka;
-using Confluent.Kafka.SyncOverAsync;
-using Microsoft.Extensions.Logging;
-using Confluent.SchemaRegistry.Serdes;
 using Confluent.SchemaRegistry;
-using Avro;
-using Weather.DTO;
+using Confluent.SchemaRegistry.Serdes;
+using Microsoft.Extensions.Logging;
 using System;
+using Weather.DTO;
 
 namespace Weather.Producer
 {
@@ -46,31 +44,20 @@ namespace Weather.Producer
             {
                 _logger.LogInformation($"{producer.Name} producing on {topic}.");
 
-                int i = 0;
-                string text;
-                while ((text = Console.ReadLine()) != "q")
-                {
-                    //var WeatherRecord = new WeatherRecord { ID = i++, Firstname = text, Lastname = "Smithy", BirthDate = "02/12/45"};
-                    WeatherRecord weatherRecord = new WeatherRecord { City = WeatherCities.Sydney, GmtOffset = 10, Type = WeatherTypes.Rainy, Temperature = 23, Humidity = 100, WindSpeed = 23 };
-                    producer
-                        .ProduceAsync(topic, new Message<string, WeatherRecord> { Key = text, Value = weatherRecord })
-                        .ContinueWith(task =>
+                WeatherRecord weatherRecord = new WeatherRecord { City = WeatherCities.Sydney, GmtOffset = 10, Type = WeatherTypes.Rainy, Temperature = 23, Humidity = 100, WindSpeed = 23 };
+                producer
+                    .ProduceAsync(topic, new Message<string, WeatherRecord> { Key = "weatherrecord", Value = weatherRecord })
+                    .ContinueWith(task =>
+                    {
+                        if (!task.IsFaulted)
                         {
-                            if (!task.IsFaulted)
-                            {
-                                Console.WriteLine($"produced to: {task.Result.TopicPartitionOffset}");
-                            }
-
-                            // Task.Exception is of type AggregateException. Use the InnerException property
-                            // to get the underlying ProduceException. In some cases (notably Schema Registry
-                            // connectivity issues), the InnerException of the ProduceException will contain
-                            // additional information pertaining to the root cause of the problem. Note: this
-                            // information is automatically included in the output of the ToString() method of
-                            // the ProduceException which is called implicitly in the below.
-                            Console.WriteLine($"error producing message: {task.Exception.InnerException}");
-                        });
-
-                }
+                            _logger.LogInformation($"produced to: {task.Result.TopicPartitionOffset}");
+                        }
+                        else
+                        {
+                            _logger.LogError($"error producing message: {task.Exception.InnerException}");
+                        }
+                    });
             }
         }
     }
